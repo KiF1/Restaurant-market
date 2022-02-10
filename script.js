e297b5d7edcdec17c9a066f971829ebc3af073b8
Abner Santos <abner.kif1@gmail.com>
2022-02-10 14:30:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const button = document.getElementById("switch");
+const head = document.getElementById("switch")
+
+button.addEventListener("change", () =>{
+  document.body.classList.toggle("dark")
+
+})
+
+
+
+
+
+
+
